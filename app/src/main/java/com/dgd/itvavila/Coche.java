c70d7003618fc38b9537a81bc3358339f9e6fbad
Diego' 'González Díaz <9.326@amav.net>
2023-01-12 18:34:51 +0100

    Initial commit

---
+++ app/src/main/java/com/dgd/itvavila/Coche.java
@@ -0,0 +1,19 @@
+package com.dgd.itvavila;
+
+public class Coche {
+    
+    private String parametro1;
+    private String parametro2;
+
+    public Coche(){
+
+    }
+
+    public Coche(String parametro1){
+
+    }
+
+    public Coche(String parametro1, String parametro2){
+
+    }
+}
---
+++ app/src/main/java/com/dgd/itvavila/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dgd.itvavila;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itvavila.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
